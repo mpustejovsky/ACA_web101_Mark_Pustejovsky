a8b99437053c9ae1bf7c8f169e877825827dd9fa
mpustejovsky <mpustejovsky@shermco.com>
2020-04-30 21:07:03 -0500

    adding new contact page

---
+++ Contact/main.js
@@ -0,0 +1,18 @@
+
+
+let namex = null
+
+
+function myFunction() {
+  alert (namex + " Thank you! I will get back to you within a few days")
+  
+  
+  /*document.getElementById("clearme").innerHTML = "Calculator Cleared" 
+  document.getElementById("result").innerHTML = "Results Will Display Here" */
+}
+
+const saveFirstName = (jjj) => {
+  namex=jjj
+  console.log (namex)
+
+}

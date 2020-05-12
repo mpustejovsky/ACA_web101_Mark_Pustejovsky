c9d788e44d4f3eb7aa30f5df7a2f4351cea22a75
mpustejovsky <mpustejovsky@yahoo.com>
2020-05-12 10:16:21 -0500

    updated interactive resume

---
+++ Resume/resume-interactive.js
@@ -0,0 +1,27 @@
+
+
+
+let changecolor = ()=>{
+
+  $("#dak").fadeOut();
+  $("#dak").fadeIn(5000)
+  
+}
+
+let hideImage = () => {
+
+   $("#image_dak").fadeOut()
+   
+}
+
+let replaceImage = ()=> {
+
+    $("#image_dak").fadeIn()
+  
+}
+
+let displayphone = () =>{
+    console.log("hello")
+    document.getElementById ("displaynum").innerHTML= "1-800-COWBOY"
+  
+}
